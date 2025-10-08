01a5a86f05b76377954d0fea33b6d2ec84f699f7
sarveshdhope <89177760+dhopesarvesh@users.noreply.github.com>
2025-10-08 12:07:27 +0530

    Delete CICDExample directory

--- CICDExample/Backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 5001;
-
-app.get('/api/message', (req, res) => {
-  res.json({ message: "Hello from the backend via CI/CD pipeline!" });
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
